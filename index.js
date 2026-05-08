2a29998c51bfe1f3fb259386dc35641c16517c0c
Juan <j.thegunner@gmail.com>
2026-05-08 16:53:22 -0300

    metodo GET agregado

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hola, mundo")
+//GET || GET product<id> || POST ||DELETE product<id>
+//FUNCIONES
+
+async function get(end){
+    try {
+        const promesa= await fetch(url+end)
+        const data= await promesa.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const url= "https://fakestoreapi.com/"
+const args= process.argv
+//const {,, metodo, endpoint}= args
+const metodo = args[2]
+const endpoint= args[3]
+
+switch (metodo.toUpperCase()){
+    case "GET":
+        get(endpoint)
+        break
+    case "POST":
+        console.log("POST")
+        break
+    case "DELETE":
+        console.log("DELETE")
+        break    
+}
+
